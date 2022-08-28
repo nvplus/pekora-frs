6a3d22daf8fb4c64dc1437a88edad54bd30fb2a8
Vincent Nguyen <v.ngu@outlook.com>
2022-08-27 23:06:34 -0400

    update code to suck less

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Pekora FRS
 Pekora FRS
-
 ## Pekora FRS
-1. Start video server on localhost:3000: ``cd server`` and ``npm start``
+1. Start video server: ``cd server`` and ``npm start``
 3. Open player in ``client/client.html``
--- server/server.js
+++ server/server.js
@@ -2,27 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.get('/video/', (req, res) => {
-    const filePath = "video/output.m3u8"
-
-    fs.readFile(filePath, (error, content) => {
-        if (error){
-            res.writeHead(500);
-            res.end(); 
-        }
-        else {
-            res.writeHead(200, {
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "vnd.apple.mpegURL"
-            });
-
-            res.end(content, "utf-8")
-        }
-    });
-});
-
-app.get('/video/:sequence', (req, res) => {
-    const filePath = "./video/" + req.params.sequence;
+app.get('/video/:sequence?', (req, res) => {
+    let filePath = "video/output.m3u8";
+    if (req.params.sequence){
+        filePath = "./video/" + req.params.sequence;
+    }
+    
     fs.readFile(filePath, (error, content) => {
         if (error){
             res.writeHead(500);
